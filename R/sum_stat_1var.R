0efa5543d3382d21215dc9d4d8d56cca757db977
tadhg.m <tadhg.moore@dkit.ie>
2019-05-23 15:36:50 +0100

    added in na.rm to prevent NA's in data

--- R/sum_stat_1var.R
+++ R/sum_stat_1var.R
@@ -13,15 +13,15 @@
 sum_stat_1var <- function(mod, obs,na.rm =T){
   dif = mod- obs
   pear_r = cor.test(obs, mod, method = 'pearson')
-  var_obs = mean(((obs-mean(obs))^2))
-  var_mod = mean(((mod-mean(mod))^2))
+  var_obs = mean(((obs-mean(obs))^2), na.rm = na.rm)
+  var_mod = mean(((mod-mean(mod))^2), na.rm = na.rm)
   SD_obs = sd(obs, na.rm = na.rm)
   SD_mod = sd(mod, na.rm = na.rm)
-  cov = mean((obs-mean(obs))*(mod-mean(mod)))
+  cov = mean((obs-mean(obs, na.rm = na.rm))*(mod-mean(mod, na.rm = na.rm)), na.rm = na.rm)
   cor = cov/sqrt(var_obs*var_mod)
-  bias = mean(dif)
-  mae = mean(abs(dif))
-  rmse = sqrt(mean(dif^2))
+  bias = mean(dif, na.rm = na.rm)
+  mae = mean(abs(dif), na.rm = na.rm)
+  rmse = sqrt(mean(dif^2, na.rm = na.rm))
   nse = NSE(mod, obs)
   summary_stats = data.frame(Pearson_r = pear_r$estimate,Variance_obs = var_obs,
                              Variance_mod = var_mod,SD_obs = SD_obs, SD_mod = SD_mod,  Covariance = cov,
