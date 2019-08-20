83270e1a2160b69b2a9b46f512c43ffb651b14c0
tadhg.m <tadhg.moore@dkit.ie>
2019-08-20 12:58:57 +0100

    Updated init_prof.R to read in csv files and files with headers

--- R/init_prof.R
+++ R/init_prof.R
@@ -5,11 +5,13 @@
 #' @param obs_file filepath; Path to observation file
 #' @param date character; Date in "YYYY-mm-dd HH:MM:SS" format to extract the initial profile.
 #' @param tprof_file filepath; For the new initial temperature profile file.
+#' @param print logical; Prints the temperature profile to the con
+#' @param ... arguments to be passed to read.delim() for reading in observed file e.g "header = TRUE, sep = ','"
 #' @return Message stating if the file has been created
 #' @import utils
 #' @export
-init_prof <- function(obs_file, date, tprof_file){
-  obs <- read.delim(obs_file, header = F)
+init_prof <- function(obs_file, date, tprof_file, print = TRUE, ...){
+  obs <- read.delim(obs_file, stringsAsFactors = F, ...)
   #obs[,1] <- as.POSIXct(obs[,1], tz = 'UTC')
   dat = which(obs[,1] == date)
   ndeps = length(dat)
@@ -23,4 +25,7 @@ init_prof <- function(obs_file, date, tprof_file){
   write.table(df, tprof_file, quote = F, row.names = F, col.names = F,
               sep = "\t")
   message('New inital temperature file ', tprof_file, ' has been created.')
+  if(print == TRUE){
+    print(df)
+  }
 }
